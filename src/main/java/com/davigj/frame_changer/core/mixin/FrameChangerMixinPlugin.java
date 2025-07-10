1246627251a5d31c83aaa8979752f6d5d421dbc1
Cassian Godsted <cassian@godsted.com>
2025-07-10 15:48:29 -0400

    another quad fix

--- src/main/java/com/davigj/frame_changer/core/mixin/FrameChangerMixinPlugin.java
+++ src/main/java/com/davigj/frame_changer/core/mixin/FrameChangerMixinPlugin.java
@@ -21,7 +21,7 @@ public class FrameChangerMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        if (mixinClassName.contains("PortalShapeMixin") && FabricLoader.getInstance().isModLoaded("quad")) return false;
+        if ((mixinClassName.contains("PortalShapeMixin") || mixinClassName.contains("BaseFireBlock")) && FabricLoader.getInstance().isModLoaded("quad")) return false;
         return true;
     }
 
